a18ca7d80b0c7ebe39e98dfb8504b23608c97815
tranhoangthaicv <83809894+tranhoangthaicv@users.noreply.github.com>
2022-12-07 22:37:51 +0700

    dynamic router

--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -1,17 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Banner } from '../components/banner'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const handleClickBanner = () => {
+    console.log("Click banner")
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Coffee shop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Coffee Shop </h1>
+        <Banner buttonText="View coffee nearby" handleOnClick={handleClickBanner}/>
       </main>
 
     </div>
